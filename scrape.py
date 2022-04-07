d429d02a7fb06911204dd7797cf28b44eb293903
Jason Rebuck <jaschon@users.noreply.github.com>
2022-04-07 10:56:35 -0700

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -15,9 +15,6 @@ HEADERS = {
 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
 "Accept-Encoding": "gzip, deflate", 
 "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8", 
-"Dnt": "1", 
-"Host": "httpbin.org", 
-"Upgrade-Insecure-Requests": "1", 
 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36", 
 }
 
